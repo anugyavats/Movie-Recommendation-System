be9b9e4202150c798255a6a6a1a17248666b7181
Anugya Vats <111658328+anugyavats@users.noreply.github.com>
2023-05-28 23:35:20 +0530

    Update MRSapp.py

--- MRSapp.py
+++ MRSapp.py
@@ -13,6 +13,7 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
+
 def fetch_overview(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
